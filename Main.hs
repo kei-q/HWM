b8abdb543d5fda1e126d912073543a0c80134f41
keqh <keqh.keqh@gmail.com>
2010-05-30 02:00:37 +0900

    fix: shellhook process

--- Main.hs
+++ Main.hs
@@ -11,9 +11,10 @@ import FRP.Reactive
 import FRP.Reactive.LegacyAdapters
 import Control.Concurrent (forkIO)
 import Control.Applicative ((<$>))
-import Control.Monad (mplus, filterM)
+import Control.Monad (mplus)
 import Data.List (delete)
-
+import Foreign.Ptr (plusPtr, nullPtr)
+import Data.Bits ((.&.))
 import Windows
 
 mOD_ALT = 1
@@ -22,84 +23,83 @@ mOD_SHIFT = 4
 mOD_WIN = 8
 wM_HOTKEY = 0x312
 
-foreign import stdcall unsafe "windows.h SetForegroundWindow"
-  setForegroundWindow :: HWND -> IO HWND
-
 main :: IO ()
 main = do
-  let clsName = mkClassName "My Window Class"
-  hinst <- getModuleHandle Nothing
-  _     <- registerClass (cS_DBLCLKS, hinst, Nothing, Nothing, Nothing, Nothing, clsName)
-  shi   <- registerWindowMessage "SHELLHOOK" -- registerShellHookWindowのメッセージがながれるようにする
-
   t <- makeClock
   (shellSink, shellEvent) <- makeEvent t
-  (keySink, keyEvent) <- makeEvent t
+  (keySink, keyEvent)     <- makeEvent t
+  
+  shi <- registerWindowMessage "SHELLHOOK" -- registerShellHookWindowのメッセージがながれるようにする
 
-  mainhwnd <- createWindow 
-    clsName
-    "HWM"
-    wS_THICKFRAME
-    Nothing 
-    Nothing 
-    Nothing 
-    Nothing 
-    Nothing 
-    Nothing 
-    hinst
-    (wndProc shi shellSink keySink)
+  mainhwnd <- simpleInitWindow "HWM" (wndProc shi shellSink keySink)
 
-  -- registerHotKey mainhwnd 0xAFAF mOD_CONTROL vK_LEFT
-  registerHotKey mainhwnd 0x10 3 0x41 -- Alt+Ctrl+a
-  registerHotKey mainhwnd 0x20 3 0x42 -- Alt+Ctrl+b
+  registerHotKeys mainhwnd
 
   -- shellhookの登録に成功したら実行
   whenM (registerShellHookWindow mainhwnd) $ do
-    hwnds <- enumWindows >>= filterM filterHWND
-    mapM_ printHWNDInfo hwnds
+    hwnds <- enumWindows
+    mapM printHWNDInfo hwnds
 
-    -- forkIO $ adaptE $ putStrLn <$> (fmap show shellEvent) `mplus` (fmap show keyEvent)
     forkIO $ adaptE $ action <$> (scanlE aux (W [] hwnds) $ (fmap convShell shellEvent) `mplus` (fmap convKey keyEvent))
     allocaMessage pump
 
-convShell = snd
-convKey = id
-aux (W l []) 0x10 = W [] (reverse l)
-aux (W l r) 0x10 = W (head r : l) (tail r)
-aux (W [] r) 0x20 = W (reverse r) []
-aux (W l r) 0x20 = W (tail l) (head l : r)
-aux hwnds 2 = hwnds
-aux hwnds 4 = hwnds
-aux hwnds 6 = hwnds
-aux hwnds _ = hwnds
-action hwnds@(W _ (r:_)) = print hwnds >> setForegroundWindow r >>= print >> return ()
+registerHotKeys :: HWND -> IO ()
+registerHotKeys hwnd = do
+  -- registerHotKey mainhwnd 0xAFAF mOD_CONTROL vK_LEFT
+  registerHotKey hwnd 0x10 3 0x41 -- Alt+Ctrl+a
+  registerHotKey hwnd 0x20 3 0x42 -- Alt+Ctrl+b
+  return ()
+  
+
+convShell (4,hwnd) = flip focus $ hwnd
+convShell (2,hwnd) = flip addW $ hwnd
+convShell (1,hwnd) = flip removeW $  hwnd
+convShell _ = id
+convKey 0x10 = rotateL
+convKey 0x20 = rotateR
+
+aux w f = f w
+
+rotateL (W l []) = W [] (reverse l)
+rotateL (W l r)  = W (head r : l) (tail r)
+rotateR (W [] r) = W (reverse r) []
+rotateR (W l r)  = W (tail l) (head l : r)
+
+focus :: Windows -> HWND -> Windows
+focus (W l r) hwnd
+  | elem hwnd l = W (delete hwnd l) (hwnd : r)
+  | elem hwnd r = W l (hwnd : delete hwnd r)
+  | otherwise = W l r
+
+addW :: Windows -> HWND -> Windows
+addW (W l r) hwnd = W [] (hwnd : (reverse l) ++ r)
+
+removeW :: Windows -> HWND -> Windows
+removeW (W l r) hwnd
+  | elem hwnd l = W (delete hwnd l) r
+  | elem hwnd r = W l (delete hwnd r)
+  | otherwise = W l r
+
+action hwnds@(W _ (r:_)) = print hwnds >> setForegroundWindow r >> return ()
 action hwnds = print hwnds
 
 data Windows = W [HWND] [HWND] deriving Show
 
-filterHWND hwnd = do
-  title <- getWindowText hwnd
-  return $ title /= "" && title /= "Program Manager"
-  
-
-pump :: LPMSG -> IO ()
-pump lpmsg = whenM (getMessage lpmsg Nothing) $ do
-  _ <- translateMessage lpmsg
-  _ <- dispatchMessage lpmsg
-  pump lpmsg
-
 -- wndProc :: WindowMessage -> HWND -> WindowMessage -> WPARAM -> LPARAM -> IO LRESULT
 wndProc shellhookid shellSink keySink hwnd msg wp lp
-  | msg == wM_HOTKEY = keyProc
-  | shellhookid /= msg = defWindowProc (Just hwnd) msg wp lp
-  | otherwise = wndProc' -- registerWindowMessageで登録した値を比較し、shellhook関係か確認
+  | msg == wM_HOTKEY   = keyProc
+  | msg == shellhookid = shellProc -- registerWindowMessageで登録した値を比較し、shellhook関係か確認 
+  | otherwise = defWindowProc (Just hwnd) msg wp lp
   where
-    keyProc = do
-      keySink wp
-      return 0
-    wndProc' = do
-      shellSink (lp,wp) -- '4'はアクティブウィンドウが切り替わったとき
-      return 0
+    -- wp -> keyID
+    keyProc = nilAct $ keySink wp
+    -- lp -> message
+    -- wp -> HWND
+    shellProc = let hwnd = castUINTToPtr $ toEnum $ fromEnum lp
+                in do b <- (isWindowVisible hwnd)
+		      if b
+		        then nilAct $ shellSink (wp .&. 0x7fff, hwnd)
+		        else return 0
 
 printHWNDInfo :: HWND -> IO ()
 printHWNDInfo hwnd = do
@@ -113,3 +113,5 @@ printHWNDInfo hwnd = do
 -- util
 whenM :: Monad m => m Bool -> m () -> m ()
 whenM test act = test >>= \t -> if t then act else return ()
+
+nilAct act = act >> return 0
--- Windows.hs
+++ Windows.hs
@@ -6,12 +6,55 @@ module Windows
     , registerHotKey
     , getWindowText
     , enumWindows
+    , setForegroundWindow
+    
+    -- util
+    , simpleInitWindow
+    , pump
     ) where
 
+
+import System.Win32.DLL
+import Graphics.Win32
 import Graphics.Win32.GDI.Types 
 import System.Win32.Types
 import Foreign.Marshal.Array (peekArray)
 import Foreign.Ptr (Ptr)
+
+import Control.Monad (filterM)
+
+simpleInitWindow :: String -> WindowClosure -> IO HWND
+simpleInitWindow title proc = do
+  let clsName = mkClassName "My Window Class"
+  hinst <- getModuleHandle Nothing
+  _     <- registerClass (cS_DBLCLKS, hinst, Nothing, Nothing, Nothing, Nothing, clsName)
+
+  createWindow 
+    clsName
+    title
+    wS_THICKFRAME
+    Nothing 
+    Nothing 
+    Nothing 
+    Nothing 
+    Nothing 
+    Nothing 
+    hinst
+    proc
+
+pump :: LPMSG -> IO ()
+pump lpmsg = do
+  res <- (getMessage lpmsg Nothing)
+  if res
+    then do
+      _ <- translateMessage lpmsg
+      _ <- dispatchMessage lpmsg
+      pump lpmsg
+    else return ()
+
+------------------------------------------------------------
+-- foreign imports
+------------------------------------------------------------
 
 foreign import stdcall unsafe "windows.h RegisterShellHookWindow"
   registerShellHookWindow :: HWND -> IO BOOL
@@ -25,7 +68,10 @@ foreign import stdcall unsafe "windows.h IsWindow"
 foreign import stdcall unsafe "windows.h RegisterHotKey"
   registerHotKey :: HWND -> INT -> UINT -> UINT -> IO BOOL
 
-------------------------------------------------------------
+foreign import stdcall unsafe "windows.h SetForegroundWindow"
+  setForegroundWindow :: HWND -> IO HWND
+
+----------------------------------------
 -- getWindowText
 foreign import stdcall unsafe "windows.h GetWindowTextW"
   c_getWindowText :: HWND -> LPCTSTR -> INT -> IO INT
@@ -36,7 +82,7 @@ getWindowText hwnd = do
     c_getWindowText hwnd buf 256
     peekTString buf
 
-------------------------------------------------------------
+----------------------------------------
 -- enumWindows
 foreign import ccall "enumWindows.h enumWindows"
   c_enumWindows :: IO (Ptr HWND)
@@ -48,5 +94,8 @@ enumWindows :: IO [HWND]
 enumWindows = do
   p_hwnds <- c_enumWindows
   hwndsLength <- c_hwndLength
-  peekArray hwndsLength p_hwnds
+  hwnds <- peekArray hwndsLength p_hwnds
+  filterM (\hwnd -> do
+    title <- getWindowText hwnd
+    return $ title /= "" && title /= "Program Manager") hwnds
 
