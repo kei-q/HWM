78ab05fe12905eca76f930c52c5fe7c631f1394c
keqh <keqh.keqh@gmail.com>
2010-06-01 23:41:18 +0900

    refactoring

--- Main.hs
+++ Main.hs
@@ -34,39 +34,25 @@ main :: IO ()
 main = do
   t <- makeClock
   (shellSink, shellEvent) <- makeEvent t
-  (keySink, keyEvent)     <- makeEvent t
-  
-  shi <- registerWindowMessage "SHELLHOOK" -- registerShellHookWindowのメッセージがながれるようにする
+  (keySink  , keyEvent)   <- makeEvent t
+
+  shi <- registerWindowMessage "SHELLHOOK" 
 
   mainhwnd <- simpleInitWindow "HWM" (wndProc shi shellSink keySink)
 
   registerHotKeys mainhwnd
 
-  -- shellhookの登録に成功したら実行
   whenM (registerShellHookWindow mainhwnd) $ do
     hwnds <- enumWindows
-    mapM printHWNDInfo hwnds
-
-    forkIO $ adaptE $ action <$> (scanlE aux (newWM hwnds) $ (fmap convShell shellEvent) `mplus` (fmap convKey keyEvent))
+    forkIO $ adaptE $ action <$> mainEvent hwnds shellEvent keyEvent
     allocaMessage pump
 
 registerHotKeys :: HWND -> IO ()
 registerHotKeys hwnd = forM_ keys (curry3 $ registerHotKey hwnd)
 
-convShell (4,hwnd) = flip focus  $ hwnd
-convShell (2,hwnd) = flip add    $ hwnd
-convShell (1,hwnd) = flip remove $ hwnd
-convShell _ = id
-convKey 0x10 = rotateL
-convKey 0x20 = rotateR
-
-aux w f = WM { wmStack = f (wmStack w) }
-
-action wm = print wm >> setForegroundWindow (getActive $ wmStack wm) >> return ()
-
 wndProc shellhookid shellSink keySink hwnd msg wp lp
   | msg == wM_HOTKEY   = keyProc
-  | msg == shellhookid = shellProc -- registerWindowMessageで登録した値を比較し、shellhook関係か確認 
+  | msg == shellhookid = shellProc
   | otherwise = defWindowProc (Just hwnd) msg wp lp
   where
     -- wp -> keyID
@@ -79,13 +65,18 @@ wndProc shellhookid shellSink keySink hwnd msg wp lp
 		        then nilAct $ shellSink (wp .&. 0x7fff, hwnd)
 		        else return 0
 
-printHWNDInfo :: HWND -> IO ()
-printHWNDInfo hwnd = do
-  rect <- getWindowRect hwnd
-  print rect
-  getWindowText hwnd >>= print
-  print hwnd
+mainEvent hwnds shellEvent keyEvent = (scanlE aux (newWM hwnds) $ (fmap convShell shellEvent) `mplus` (fmap convKey keyEvent))
 
+convShell (4,hwnd) = flip focus  $ hwnd
+convShell (2,hwnd) = flip add    $ hwnd
+convShell (1,hwnd) = flip remove $ hwnd
+convShell _ = id
+convKey 0x10 = rotateL
+convKey 0x20 = rotateR
+
+aux w f = WM { wmStack = f (wmStack w) }
+
+action wm = print wm >> setForegroundWindow (getActive $ wmStack wm) >> return ()
 
 ------------------------------------------------------------
 -- util
