0b5073899c0f06c00229619f9751f75a0aed6d38
keqh <keqh.keqh@gmail.com>
2010-06-02 22:51:44 +0900

    refactoring around events

--- Main.hs
+++ Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE PatternGuards #-}
 module Main where
 
 import System.Win32.Types
@@ -15,6 +16,7 @@ import Control.Monad (mplus)
 import Foreign.Ptr (plusPtr, nullPtr)
 import Data.Bits ((.&.))
 import Data.Char (ord)
+import Data.List (find)
 
 import Windows
 import qualified Stack as S
@@ -30,6 +32,10 @@ keys = [ (0x10,3,0x41) -- Ctrl + Shift + A
        , (0x20,3,0x42) -- Ctrl + Shift + B
        ]
 
+maps = [ (0x10,S.rotateL)
+       , (0x20,S.rotateR)
+       ]
+
 main :: IO ()
 main = do
   t <- makeClock
@@ -41,11 +47,21 @@ main = do
   mainhwnd <- simpleInitWindow "HWM" (wndProc shi shellSink keySink)
 
   registerHotKeys mainhwnd
+  let shellE = fmap convShell shellEvent
+  let keyE = fmap (convKey maps) keyEvent
 
   whenM (registerShellHookWindow mainhwnd) $ do
-    let act e = e shellEvent keyEvent
-    runMainThread act
+    runMainThread $ shellE `mplus` keyE
     allocaMessage pump
+  where
+    convKkey maps keyID
+      | Just (_,f) <- find ((==keyID).fst) maps = f
+      | otherwise = id
+
+convShell (4,hwnd) = flip S.focus  $ hwnd
+convShell (2,hwnd) = flip S.add    $ hwnd
+convShell (1,hwnd) = flip S.remove $ hwnd
+convShell _ = id
 
 registerHotKeys :: HWND -> IO ()
 registerHotKeys hwnd = forM_ keys (curry3 $ registerHotKey hwnd)
@@ -63,24 +79,14 @@ wndProc shellhookid shellSink keySink hwnd msg wp lp
                   nilAct $ shellSink (wp .&. 0x7fff, hwnd)
       where convLPARAM2HWND = castUINTToPtr . toEnum . fromEnum 
 
-runMainThread act = do
+runMainThread e = do
   hwnds <- enumWindows
-  forkIO $ adaptE $ action <$> act (mainEvent hwnds)
+  forkIO $ adaptE $ action <$> scanlE (flip S.updateStack) (S.newWM hwnds) e
   where
-    mainEvent hwnds shellEvent keyEvent
-      = (scanlE aux (S.newWM hwnds) $ (fmap convShell shellEvent)
-                                      `mplus` (fmap convKey keyEvent))
+    action wm = print wm >> setForegroundWindow (S.getActive $ S.getStack wm) >> return ()
 
-convShell (4,hwnd) = flip S.focus  $ hwnd
-convShell (2,hwnd) = flip S.add    $ hwnd
-convShell (1,hwnd) = flip S.remove $ hwnd
-convShell _ = id
-convKey 0x10 = S.rotateL
-convKey 0x20 = S.rotateR
 
-aux = flip S.updateStack
 
-action wm = print wm >> setForegroundWindow (S.getActive $ S.getStack wm) >> return ()
 
 ------------------------------------------------------------
 -- util
